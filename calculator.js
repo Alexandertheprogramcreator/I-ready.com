74fdcea87f459d9e49e42371a5af05c8ac333803
Alexandertheprogramcreator <alexanderjsanders@gmail.com>
2026-02-23 20:11:38 -0500

    Fix 404 by renaming main file to index.html

--- calculator.js
+++ calculator.js
@@ -18,6 +18,6 @@ function calculate() {
         display.value = eval(display.value); 
     } catch (error) {
         display.value = "Error";
-        window.alert("Alexander Sanders the king of math and programming formulas does not approve of this mathematical incompetence and a transgression to the power of the homemade calculator and does not behave kindly to those who mock the power of the calculator. Use this ability appropriately - Alexander Sanders");
+        window.alert("Alexander Sanders the king of math and programming formulas does not approve of this mathematical incompetence and a transgression to the power of the homemade calculator and does not behave kindly to those who mock the power of the homemade calculator. Use this ability appropriately, All hail Alexander Sanders - Alexander Sanders");
     }
 }
